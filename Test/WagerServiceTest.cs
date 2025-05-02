6569bde92be8699c5db02a23785ae764bc6d163a
selharrak <soumia.elharrak@complevo.de>
2025-05-02 15:28:09 +0200

    Add test

--- Test/WagerServiceTest.cs
+++ Test/WagerServiceTest.cs
@@ -1,11 +1,9 @@
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
-using Microsoft.VisualBasic;
 using Moq;
 using Wager;
 using Wager.Data;
 using Wager.Services;
-using Xunit.Repeat;
 
 namespace Test
 {
@@ -15,7 +13,7 @@ namespace Test
         private Mock<ILogger<WagerService>> _logger;
         private Mock<IMemoryCache> _cache;
         private ICacheEntry _cachEntry = Mock.Of<ICacheEntry>();
-        static long Account = 10000;
+        // static long Account = 10000;
         public WagerServiceTest()
         {
             _executionContext = new Mock<IExecutionContext>(MockBehavior.Strict);
@@ -24,37 +22,49 @@ namespace Test
             _cache.Setup(m => m.CreateEntry(It.IsAny<object>())).Returns(_cachEntry);
         }
 
-        [Theory()]
-        [Repeat(30)]
-        public void Wager(int iterationNumber)
+        [Fact]
+        public void Wager()
         {
             // Arrange
             var playerId = Guid.NewGuid();
+            long account = 10000;
             _executionContext.Setup(x => x.PlayerId).Returns(playerId);
-            _cachEntry.Value = Account;
+            ParallelOptions parallelOptions = new() { MaxDegreeOfParallelism = 1 };
+            SetAccountInCache(playerId, account);
+            var service = new WagerService(_executionContext.Object, _cache.Object, _logger.Object);
+            Parallel.For(0, 30, parallelOptions, i =>
+            {
+               
+                var dto = new BetDto { Number = 3, Points = 100 };
+                // Act
+                var result = service.Bet(dto);
+
+                // Assert
+                Assert.IsType<BetResultDto>(result);
+                if (result.Status == ResultStatus.Won)
+                {
+                    Assert.Equal(account + 9 * dto.Points, result.Account);
+                }
+                else
+                {
+                    Assert.Equal(account - dto.Points, result.Account);
+                }
+
+                account = result.Account;
+                SetAccountInCache(playerId, account);
+            });
+
+
+        }
+
+        private void SetAccountInCache(Guid playerId, long account)
+        {
             _cache.Setup(x => x.TryGetValue(playerId, out It.Ref<object>.IsAny))
                 .Callback((object key, out object value) =>
                 {
-                    value = Account;
+                    value = account;
                 })
             .Returns(true);
-            var dto = new BetDto { Number = 3, Points = 100 };
-            // Act
-            var service = new WagerService(_executionContext.Object, _cache.Object, _logger.Object);
-            var result = service.Bet(dto);
-
-            // Assert
-            Assert.IsType<BetResultDto>(result);
-            if (result.Status == ResultStatus.Won)
-            {
-                Assert.Equal(Account + 9 * dto.Points, result.Account);
-            }
-            else
-            {
-                Assert.Equal(Account - dto.Points, result.Account);
-            }
-
-            Account = result.Account;
         }
     }
 }
